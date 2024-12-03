cfddc3565354bf9f26d55dbe037f4444d089fe00
reboound <al@wolfsburg.de>
2024-12-03 11:29:48 +0100

    Added exe and adjusted Main.hs

--- SudokuCreator.hs
+++ SudokuCreator.hs
@@ -87,19 +87,3 @@ matricesEqual m1 m2 =
 
 matrixToIntList :: Matrix -> [Int]
 matrixToIntList m = concatMap matrixRowToIntList (matrixToRowList m)
-
-
-
-
-main :: IO ()
-main = do
-    putStrLn "Generiertes Sudoku:"
-    sudoku <- generateUniqueSudoku
-    printSudokuMatrix sudoku
-
-    putStrLn "\nDrücken Sie Enter, um das gelöste Sudoku anzuzeigen..."
-    _ <- getLine
-
-    putStrLn "\nGelöstes Sudoku:"
-    let solvedSudoku = backtracking sudoku (MatrixPosition (0, 0))
-    printSudokuMatrix solvedSudoku
